0a5974f120e1d882f5d680d776d96f5494ad2168
Boubacar Traore <bt.boubacar@gmail.com>
2023-09-10 12:42:14 +0200

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <header>
-      <p>This is a header</p>
-    </header>
+    <>
+      <header>
+        <p>This is a header</p>
+      </header>
+      <Footer />
+    </>
   );
 }
 
